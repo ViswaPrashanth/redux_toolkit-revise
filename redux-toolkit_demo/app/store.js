1b0d12470d6f3a3aeb656d76c08b6e627e181729
ViswaPrasanth <85486741+ViswaPrashanth@users.noreply.github.com>
2022-12-16 08:13:05 +0530

    adding extra reducers

--- redux-toolkit_demo/app/store.js
+++ redux-toolkit_demo/app/store.js
@@ -1,10 +1,12 @@
-const configureStore = require("@reduxjs/toolkit").configureStore
-const cakeReducer = require("../features/cake/cakeSlice")
+const configureStore = require("@reduxjs/toolkit").configureStore;
+const cakeReducer = require("../features/cake/cakeSlice");
+const icecreamReducer = require("../features/icecream/icecreamSlice");
 
 const store = configureStore({
-    reducer : {
-        cake : cakeReducer
-    }
-})
+  reducer: {
+    cake: cakeReducer,
+    icecream: icecreamReducer,
+  },
+});
 
-module.exports = store
+module.exports = store;
